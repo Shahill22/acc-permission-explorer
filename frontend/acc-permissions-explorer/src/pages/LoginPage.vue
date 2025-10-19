2b52c0452a26f1ecebaccb671c501c0b5017ceca
Shahil <shahill35.ms@gmail.com>
2025-10-19 23:46:36 +0530

    login page

---
+++ frontend/acc-permissions-explorer/src/pages/LoginPage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="q-pa-xl flex flex-center column">
+    <q-card class="q-pa-lg" style="min-width: 320px">
+      <div class="text-h6 q-mb-md">ACC Permissions Explorer</div>
+      <q-btn label="Sign in with Autodesk" color="primary" @click="signin" />
+    </q-card>
+  </div>
+</template>
+
+<script setup>
+import http from 'src/lib/http'
+const signin = async () => {
+  const { data } = await http.get('/auth/login')
+  window.location.href = data.auth_url // redirects to Autodesk OAuth
+}
+</script>
